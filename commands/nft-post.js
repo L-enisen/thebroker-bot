bdde60c00ef1daa0c6253a8652ac65b64b729de6
L-enisen <sg@lenisen.com>
2025-08-23 14:23:21 +0200

    added new-lore command

--- commands/nft-post.js
+++ commands/nft-post.js
@@ -1,32 +1,51 @@
 // 📁 commands/nft-post.js
-const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionFlagsBits,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
+} = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('nft-post')
-    .setDescription('Post a new NFT preview')
+    .setDescription('Post a new Chaotic Icons NFT with OpenSea link')
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-    .addStringOption(opt => opt.setName('pack').setDescription('Pack name').setRequired(true))
-    .addStringOption(opt => opt.setName('nft').setDescription('NFT name').setRequired(true))
-    .addStringOption(opt => opt.setName('image').setDescription('Image URL').setRequired(true))
-    .addStringOption(opt => opt.setName('meta1').setDescription('Metadata 1'))
-    .addStringOption(opt => opt.setName('meta2').setDescription('Metadata 2')),
+    .addStringOption(opt =>
+      opt.setName('pack')
+        .setDescription('Pack name')
+        .setRequired(true)
+    )
+    .addStringOption(opt =>
+      opt.setName('nft')
+        .setDescription('NFT name')
+        .setRequired(true)
+    )
+    .addStringOption(opt =>
+      opt.setName('opensea')
+        .setDescription('OpenSea link')
+        .setRequired(true)
+    ),
+
   async execute(interaction) {
     const pack = interaction.options.getString('pack');
     const nft = interaction.options.getString('nft');
-    const image = interaction.options.getString('image');
-    const meta1 = interaction.options.getString('meta1');
-    const meta2 = interaction.options.getString('meta2');
+    const opensea = interaction.options.getString('opensea');
 
     const embed = new EmbedBuilder()
-      .setTitle(`🧬 ${nft} – ${pack}`)
-      .setImage(image)
-      .addFields(
-        meta1 ? { name: 'Metadata 1', value: meta1, inline: true } : {},
-        meta2 ? { name: 'Metadata 2', value: meta2, inline: true } : {}
-      )
-      .setColor(0x2ecc71);
+      .setTitle('🧬 New NFT Released')
+      .setDescription(`📦 **Pack:** ${pack}\n🎨 **NFT:** ${nft}`)
+      .setColor(0x8e44ad);
 
-    await interaction.reply({ embeds: [embed] });
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel('View on OpenSea')
+        .setStyle(ButtonStyle.Link)
+        .setURL(opensea)
+    );
+
+    await interaction.reply({ embeds: [embed], components: [row] });
   }
 };
